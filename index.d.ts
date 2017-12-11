51767424d742a4a44be660a9d7bfeed6ef3e8122
Michael North <michael.l.north@gmail.com>
2017-12-11 12:33:44 -0600

    Update types based on @chriskrycho's suggestions

--- index.d.ts
+++ index.d.ts
@@ -1,61 +1,121 @@
 declare module 'ember-concurrency' {
+
   import ComputedProperty from '@ember/object/computed';
-
-  export function task(fn: (...args: any[]) => IterableIterator<any>): Task;
-  export function timeout(delay: number): Promise<void>;
-
-  type TaskState = 'running' | 'queued' | 'idle';
-  type TaskInstanceState =
-    | 'dropped'
-    | 'cancelled'
-    | 'finished'
-    | 'running'
-    | 'waiting';
-
-  interface TaskProperty extends ComputedProperty<Task> {
-    cancelOn(...eventNames: string[]): TaskProperty;
-    on(...eventNames: string[]): TaskProperty;
-    debug(): TaskProperty;
-    drop(): TaskProperty;
-    enqueue(): TaskProperty;
-    keepLatest(): TaskProperty;
-    restartable(): TaskProperty;
-    maxConcurrency(max: number): TaskProperty;
-    group(groupPath: string): TaskProperty;
+  import RSVP from 'rsvp';
+  
+  export enum TaskInstanceState {
+    Dropped = 'dropped',
+    Canceled = 'canceled',
+    Finished = 'finished',
+    Running = 'running',
+    Waiting = 'waiting',
   }
-
-  interface TaskInstance<T> extends Promise<T> {
-    readonly error?: Error;
-    readonly hasStarted: boolean;
-    readonly isCancelled?: boolean;
-    readonly isDropped?: boolean;
-    readonly isError?: boolean;
-    readonly isFinished?: boolean;
-    readonly isRunning?: boolean;
-    readonly isSuccessful?: boolean;
-    readonly state: TaskInstanceState;
-    readonly value: T;
+  
+  export interface TaskProperty<T> extends ComputedProperty<T> {
+    cancelOn(eventNames: string[]): this;
+    debug(): this;
+    drop(): this;
+    enqueue(): this;
+    group(groupPath: string): this;
+    keepLatest(): this;
+    maxConcurrency(n: number): this;
+    on(eventNames: string[]): this;
+    restartable(): this;
+  }
+  
+  export interface TaskInstance<T> extends PromiseLike<T> {
+    readonly error?: any;
+    readonly hasStarted: ComputedProperty<boolean>;
+    readonly isCanceled: ComputedProperty<boolean>;
+    readonly isDropped: ComputedProperty<boolean>;
+    readonly isError: boolean;
+    readonly isFinished: ComputedProperty<boolean>;
+    readonly isRunning: ComputedProperty<boolean>;
+    readonly isSuccessful: boolean;
+    readonly state: ComputedProperty<TaskInstanceState>;
+    readonly value?: T;
     cancel(): void;
-    finally<TResult = never>(
-      onrejected?:
-        | ((reason: any) => TResult | PromiseLike<TResult>)
-        | undefined
-        | null
-    ): Promise<T | TResult>;
+    catch(): RSVP.Promise<any>;
+    finally(): RSVP.Promise<any>;
+    then<TResult1 = T, TResult2 = never>(
+      onfulfilled?: ((value: T) => TResult1 | RSVP.Promise<TResult1>) | undefined | null,
+      onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | undefined | null
+    ): RSVP.Promise<TResult1 | TResult2>;
   }
-
-  interface Task<T = any> {
+  
+  export enum TaskState {
+    Running = 'running',
+    Queued = 'queued',
+    Idle = 'idle',
+  }
+  
+  interface Task<T, P> extends TaskProperty<T> {
     readonly isIdle: boolean;
     readonly isQueued: boolean;
     readonly isRunning: boolean;
     readonly last?: TaskInstance<T>;
-    readonly performCount: number;
-    readonly state: TaskState;
-    readonly lastCancelled?: TaskInstance<T>;
+    readonly lastCanceled?: TaskInstance<T>;
     readonly lastComplete?: TaskInstance<T>;
     readonly lastErrored?: TaskInstance<T>;
+    readonly lastIncomplete?: TaskInstance<T>;
     readonly lastPerformed?: TaskInstance<T>;
+    readonly lastRunning?: TaskInstance<T>;
+    readonly lastSuccessful?: TaskInstance<T>;
+    readonly performCount: number;
+    readonly state: TaskState;
     cancelAll(): void;
-    perform(...args: any[]): Promise<void>;
+    perform: P;
   }
+  
+  export function timeout(ms: number): RSVP.Promise<void>;
+  
+  export function task<T, A>(generatorFn: () => Iterator<T>): Task<T, () => TaskInstance<T>>;
+  
+  export function task<T, A>(
+    generatorFn: (a: A) => Iterator<T>
+  ): Task<T, (a: A) => TaskInstance<T>>;
+  
+  export function task<T, A>(
+    generatorFn: (a: A) => Iterator<PromiseLike<T>>
+  ): Task<T, (a: A) => TaskInstance<T>>;
+  
+  export function task<T, A1, A2>(
+    generatorFn: (a1: A1, a2: A2) => Iterator<T>
+  ): Task<T, (a1: A1, a2: A2) => TaskInstance<T>>;
+  
+  export function task<T, A1, A2>(
+    generatorFn: (a1: A1, a2: A2) => Iterator<PromiseLike<T>>
+  ): Task<T, (a1: A1, a2: A2) => TaskInstance<T>>;
+  
+  export function task<T, A1, A2, A3>(
+    generatorFn: (a1: A1, a2: A2, a3: A3) => Iterator<T>
+  ): Task<T, (a1: A1, a2: A2, a3: A3) => TaskInstance<T>>;
+  
+  export function task<T, A1, A2, A3>(
+    generatorFn: (a1: A1, a2: A2, a3: A3) => Iterator<PromiseLike<T>>
+  ): Task<T, (a1: A1, a2: A2, a3: A3) => TaskInstance<T>>;
+  
+  export function task<T, A1, A2, A3, A4>(
+    generatorFn: (a1: A1, a2: A2, a3: A3, a4: A4) => Iterator<T>
+  ): Task<T, (a1: A1, a2: A2, a3: A3, a4: A4) => TaskInstance<T>>;
+  
+  export function task<T, A1, A2, A3, A4>(
+    generatorFn: (a1: A1, a2: A2, a3: A3, a4: A4) => Iterator<PromiseLike<T>>
+  ): Task<T, (a1: A1, a2: A2, a3: A3, a4: A4) => TaskInstance<T>>;
+  
+  export function task<T, A1, A2, A3, A4, A5>(
+    generatorFn: (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5) => Iterator<T>
+  ): Task<T, (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5) => TaskInstance<T>>;
+  
+  export function task<T, A1, A2, A3, A4, A5>(
+    generatorFn: (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5) => Iterator<PromiseLike<T>>
+  ): Task<T, (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5) => TaskInstance<T>>;
+  
+  export function task<T, A1, A2, A3, A4, A5, A6>(
+    generatorFn: (a1: A1, a2: A2, a3: A3, a4: A4, a6: A6) => Iterator<T>
+  ): Task<T, (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5, a6: A6) => TaskInstance<T>>;
+  
+  export function task<T, A1, A2, A3, A4, A5, A6>(
+    generatorFn: (a1: A1, a2: A2, a3: A3, a4: A4, a6: A6) => Iterator<PromiseLike<T>>
+  ): Task<T, (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5, a6: A6) => TaskInstance<T>>;
 }
